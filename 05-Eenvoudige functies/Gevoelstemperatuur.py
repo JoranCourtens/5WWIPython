e1e1689e823eff3a0510bbeda1b8c76d45ebe6c8
25726 <joran.courtens@sgsintpaulus.eu>
2019-11-14 18:50:25 +0100

    Gevoelstemperatuur

--- 05-Eenvoudige functies/Gevoelstemperatuur.py
+++ 05-Eenvoudige functies/Gevoelstemperatuur.py
@@ -2,7 +2,8 @@
 
 t = float(input('Geef T: '))
 w = float(input('Geef W: '))
+w_meter = w / 3.6
 
-gevoelstemperatuur = 13.12 + (0.6215 * t) + ((0.3965 * t) - 11.37) * ((3.6 * w) ** 0.16)
+gevoelstemperatuur = 13.12 + (0.6215 * t) + ((0.3965 * t - 11.37) * pow(3.6 * w_meter, 0.16))
 
-print(str(gevoelstemperatuur))
+print((gevoelstemperatuur))
