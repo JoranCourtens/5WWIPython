a1c9f268843286a203ca5da312db36ca0df6b1d0
25726 <joran.courtens@sgsintpaulus.eu>
2020-01-09 19:40:07 +0100

    Vouwen

---
+++ 07b-Whilelus/Fietsen.py
@@ -0,0 +1,14 @@
+#invoer
+snelheid_stijn = int(input('Snelheid Stijn: '))
+snelheid_kaat = int(input('Snelheid Kaat: '))
+afstand_huizen = int(input('Afstand huizen: '))
+
+#berekening
+seconden = 0
+afstand = 0
+while afstand < afstand_huizen:
+    seconden += 1
+    afstand += (snelheid_kaat + snelheid_stijn)
+
+print('Na {} s hebben Stijn en Kaat elkaar ontmoet.'.format(seconden))
+
