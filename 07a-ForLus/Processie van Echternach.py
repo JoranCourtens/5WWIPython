63bb8b0f25259f41d73350eb90026f06bf91cb43
25726 <joran.courtens@sgsintpaulus.eu>
2020-01-09 18:26:03 +0100

    Processie van Echternach

---
+++ 07a-ForLus/Processie van Echternach.py
@@ -0,0 +1,11 @@
+#invoer
+tijd = int(input('geef de tijd: '))
+
+#berekening
+som = 0
+for i in range(tijd):
+    if i % 2 == 0:
+        som += i + 2
+    else:
+        som -= (i + 1) / 2
+print(round(som))
