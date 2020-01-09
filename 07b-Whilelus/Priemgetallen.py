8b0b61f0fc01c22e3863eb476d96c8a1655d363f
25726 <joran.courtens@sgsintpaulus.eu>
2020-01-09 19:06:25 +0100

    Priemgetallen

--- 07b-Whilelus/Priemgetallen.py
+++ 07b-Whilelus/Priemgetallen.py
@@ -1,11 +1,13 @@
 #invoer
-getal = int(input('Geef getal: '))
+getal = int(input('geef een getal: '))
+i = 2
+if getal == 1:
+    print('1 is geen priemgetal')
+else:
+    while getal % i != 0 and i != getal:
+        i += 1
 
-#berekening
-deler = 2
-while getal % deler != 0 and getal > 2:
-    deler += 1
-    if deler == getal and getal % deler == 1:
-        print('{} is een priemgetal'.format(getal))
-    elif getal % deler == 0:
-        print('{} is geen priemgetal'.format(getal))
+    if i == getal:
+        print(getal, 'is een priemgetal')
+    else:
+        print(getal, 'is geen priemgetal')
