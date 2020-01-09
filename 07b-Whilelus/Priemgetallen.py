cdee75dcab32c2ff9db89f5449133a625d6938c4
25726 <joran.courtens@sgsintpaulus.eu>
2020-01-09 18:11:00 +0100

    Rij van Fibonacci

--- 07b-Whilelus/Priemgetallen.py
+++ 07b-Whilelus/Priemgetallen.py
@@ -7,5 +7,5 @@ while getal % deler != 0 and getal > 2:
     deler += 1
     if deler == getal and getal % deler == 1:
         print('{} is een priemgetal'.format(getal))
-    elif getal % deler == 0:\
+    elif getal % deler == 0:
         print('{} is geen priemgetal'.format(getal))
