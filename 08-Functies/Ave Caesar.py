8651d7bfe18643587692f9ef78d778042715ff75
25726 <joran.courtens@sgsintpaulus.eu>
2020-01-24 10:43:52 +0100

    vermoeden van Collatz

---
+++ 08-Functies/Ave Caesar.py
@@ -0,0 +1,21 @@
+def is_letter(letter):
+    return ord('a') <= ord(letter) <= ord('z') or ord('A') <= ord(letter) <= ord('Z')
+
+
+
+def roteer_letter(letter, plaatsen):
+    #volgnummer in het alfabet bepaald van de gegeven letter
+    volgnummer_letter = min(ord(letter) % ord('a'), ord(letter) % ord('A'))
+    #volgnummer in alfabet van nieuwe letter
+    nieuw_volgnummer = (volgnummer_letter + plaatsen) % 26
+    #offset
+    offset = nieuw_volgnummer - volgnummer_letter
+    return chr(ord(letter) + offset)
+
+
+
+
+
+
+
+
