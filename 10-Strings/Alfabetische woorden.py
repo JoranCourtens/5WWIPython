9bd68f31e98196940791480a8ebecdf22b42bf76
25726 <joran.courtens@sgsintpaulus.eu>
2020-02-06 20:10:01 +0100

    Cryptografie voor dummies

---
+++ 10-Strings/Alfabetische woorden.py
@@ -0,0 +1,22 @@
+def positie_laagste_ascii(woord):
+    vorig = woord[0]
+    stap = 0
+    for i in range(len(woord)):
+        if ord(woord[i]) < ord(vorig):
+            vorig = woord[i]
+            stap = i
+
+    return stap
+
+
+def sorteer(sorteer):
+    uitkomst = ''
+    for i in range(len(sorteer)):
+        uitkomst += sorteer[(positie_laagste_ascii(sorteer))]
+        sorteer = sorteer[:positie_laagste_ascii(sorteer)] + sorteer[positie_laagste_ascii(sorteer) + 1:]
+
+    return uitkomst
+
+def is_alfabetisch(woord):
+
+    ...
---
+++ 10-Strings/Cryptografie voor dummies.py
@@ -0,0 +1,20 @@
+def versleutel_woord(woord, aantal):
+    woord = woord.upper()
+    uitkomst = ''
+    for i in range(len(woord)):
+        uitkomst += chr(ord(woord[i]) + aantal)
+
+    return uitkomst
+
+def versleutel_zin(zin, aantal):
+    zin = zin.upper()
+    uitkomst = ''
+    for i in range(len(zin)):
+        if zin[i] == ' ':
+            uitkomst += '@'
+        elif chr(ord(zin[i]) + aantal) == '@':
+            uitkomst += ' '
+        else:
+            uitkomst += versleutel_woord(zin[i], aantal)
+
+    return uitkomst
