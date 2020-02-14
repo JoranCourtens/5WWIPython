a0068eb01e46bccb1a82f306808bcb482ecc4a4e
25726 <joran.courtens@sgsintpaulus.eu>
2020-02-14 11:00:08 +0100

    YIQ

---
+++ 11-Tuples/In of uit.py
@@ -0,0 +1,3 @@
+from math import sqrt
+def binnen_of_buiten(middelpunt, co_cirkel, te_bepalen_co):
+    straal = sqrt((middelpunt[0] - middelpunt[1]))
--- 11-Tuples/YIQ.py
+++ 11-Tuples/YIQ.py
@@ -2,6 +2,6 @@ def yiq(rgb):
     yiq_1 = rgb[0] * 0.299 + rgb[1] * 0.587 + rgb[2] * 0.114
     yiq_2 = rgb[0] * 0.596 + rgb[1] * -0.274 + rgb[2] * -0.322
     yiq_3 = rgb[0] * 0.211 + rgb[1] * -0.523 + rgb[2] * 0.312
-    return (yiq_1, yiq_2, yiq_3)
+    return (round(yiq_1, 4), round(yiq_2, 4), round(yiq_3, 4))
 
 
