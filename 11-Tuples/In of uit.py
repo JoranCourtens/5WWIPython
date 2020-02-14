c51510ac3ac4c769c564d0dff10cd837f5715c8b
25726 <joran.courtens@sgsintpaulus.eu>
2020-02-14 11:11:22 +0100

    In of uit

--- 11-Tuples/In of uit.py
+++ 11-Tuples/In of uit.py
@@ -1,3 +1,14 @@
 from math import sqrt
 def binnen_of_buiten(middelpunt, co_cirkel, te_bepalen_co):
-    straal = sqrt((middelpunt[0] - middelpunt[1]))
+    straal = sqrt(pow(middelpunt[0] - co_cirkel[0], 2) + pow(middelpunt[1] - co_cirkel[1], 2))
+    afstand_middelpunt_gegevenpunt = sqrt(pow(middelpunt[0] - te_bepalen_co[0], 2) + pow(middelpunt[1] - te_bepalen_co[1], 2))
+    if straal == afstand_middelpunt_gegevenpunt:
+        plaats = 'op'
+    elif straal > afstand_middelpunt_gegevenpunt:
+        plaats = 'binnen'
+    else:
+        plaats = 'buiten'
+    afstand = afstand_middelpunt_gegevenpunt
+
+    return (plaats, round(afstand, 4))
+
