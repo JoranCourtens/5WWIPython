e5b8c857c33e2bbb421daf3a2b48608be2c972cc
25726 <joran.courtens@sgsintpaulus.eu>
2020-02-29 14:52:59 +0100

    Wandelende Getallen

---
+++ Krokusvertier/Wandelende getallen.py
@@ -0,0 +1,10 @@
+from math import sin, cos, pi
+getal = input('Geef een getal: ')
+x, y = 0, 0
+
+for i in getal:
+    if i != '.':
+        x += cos((pi / 2) - (int(i) * 0.2 * pi))
+        y += sin((pi / 2) - (int(i) * 0.2 * pi))
+
+print('Getal {} wandelt naar positie ({:.2f}, {:.2f}).'.format(getal, x, y))
